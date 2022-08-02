23f02a2512223500bb6d0d5fd85da0df4a0bc585
claunicole <claunicole.gf@gmail.com>
2022-08-01 21:29:17 -0400

    Mejora en los estilos

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
     before_action :set_product, only: [:create]
+    before_action :authenticate_user!
 
     def create
         @movie.comments.create(comment_params)
