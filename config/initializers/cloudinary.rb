cb72cca35992e1cfe49803c5f6c29f82b3836641
claunicole <claunicole.gf@gmail.com>
2022-09-11 00:40:23 -0400

    cloudinary y .env

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,4 +1,4 @@
-Cloudinary.config_from_url("cloudinary://495312469592746:IQjFN8Lzb_GxyMiiO4flqMrA5Ns@claunicole")
+Cloudinary.config_from_url(ENV['URL_KEY'])
 Cloudinary.config do |config|
   config.secure = true
 end
