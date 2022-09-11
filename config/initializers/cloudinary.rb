48058337e141d0589ad25127abc7e7ef02789443
claunicole <claunicole.gf@gmail.com>
2022-09-10 23:27:16 -0400

    cloudinary fixed

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,4 +1,4 @@
-Cloudinary.config_from_url("CLOUDINARY_URL=cloudinary://495312469592746:IQjFN8Lzb_GxyMiiO4flqMrA5Ns@claunicole")
+Cloudinary.config_from_url("cloudinary://495312469592746:IQjFN8Lzb_GxyMiiO4flqMrA5Ns@claunicole")
 Cloudinary.config do |config|
   config.secure = true
 end
